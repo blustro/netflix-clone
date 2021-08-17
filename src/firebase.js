ac5c26b443a2313cf08f83ea207524667c625f6c
Bruno Lustro <brunolustro@gmail.com>
2021-08-17 21:31:01 +0000

    Firebase Init

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8U7BSDfds6KPC9OUrUFGqR5tLPOpAHwk",
+  authDomain: "react-netflix-e1201.firebaseapp.com",
+  projectId: "react-netflix-e1201",
+  storageBucket: "react-netflix-e1201.appspot.com",
+  messagingSenderId: "463467207978",
+  appId: "1:463467207978:web:16e67b2f7f1d9d5a8f9253",
+};
+
+firebase.initializeApp(firebaseConfig);
